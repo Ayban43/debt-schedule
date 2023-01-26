66ce0e4e874e4239f7aaa7bc277612bc390c8b64
Ayban43 <aybancachuela@gmail.com>
2023-01-27 04:18:23 +0800

    buttonlink

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function ButtonLink() {
   return (
     <div className="addDebtButtonWrapper">
-      <Link to="../debt-schedule/create-debt">
+      <Link to="../create-debt">
         <button className="addDebt">Add Debt</button>
       </Link>
     </div>
